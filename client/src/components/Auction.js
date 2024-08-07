a7e29d6b91790bf4f72cdb9b181e6de389e1d9ed
Ananya-55 <ananya.upd19@gmail.com>
2024-08-07 19:03:09 +0530

    Updated 2

--- client/src/components/Auction.js
+++ client/src/components/Auction.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
+import './style.css'
 
 class Auction extends React.Component {
   constructor(props) {
--- client/src/components/UserFunctions.js
+++ client/src/components/UserFunctions.js
@@ -1,6 +1,7 @@
 
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
+import './style.css'
 
 export const register = newUser => {
   return axios
